70ed1b2ec8aeb0197720312dabd92c453c458ef8
André Nascimento <162403231+Profissionalandre@users.noreply.github.com>
2024-04-15 01:23:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso-gustavo-guanabara
+ HTML5 e CSS3 do CursoEmVideo
---
+++ javascript/curso-js/aula06/script.js
@@ -0,0 +1,9 @@
+       var nome = prompt('Qual seu nome completo? ')
+       /*var idade = Number(prompt('Qual sua idade'))*/
+       var categoria = prompt('Qual sua categoria? ')
+       var dataNasimento = prompt('Qual a data de seu nascimento?')
+
+       document.write(`<strong>Nome Completo</strong>: ${nome}<br>`)
+       document.write(`<strong>Idade:</strong> ${idade} <br>`)
+       document.write(`<strong>Categoria:</strong> ${categoria}`)
+       //document.write(2024 - dataNasimento)
